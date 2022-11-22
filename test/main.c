aaeb06e9bc9c5f41219db8a01a97c5d1bed62b7d
Harmcleff <33807817+Harmcleff@users.noreply.github.com>
2022-11-22 09:29:48 +0100

    printf

---
+++ test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "../main.h"
+
+/**
+ * main - Entry check alx code
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	_printf("Hello World");
+	printf("Hello World");
+
+	return (0);
+}
